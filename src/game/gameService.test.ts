4486429c64bdffa5469786adbb2e71eb882fb768
Derrick Marcey <demarcey@microsoft.com>
2018-08-20 17:11:59 -0400

    fixing crlfs

--- src/game/gameService.test.ts
+++ src/game/gameService.test.ts
@@ -1,190 +1,190 @@
-import { IGameAction, GameService, IActionResolver, IActionPayload, IGameContext } from "./gameService";
-
-class TestPayload implements IActionPayload {
-
-}
-
-class GameContext implements IGameContext {
-
-}
-
-const MockActionResolver = jest.fn<IActionResolver<GameContext, TestPayload>>((canUndo?: boolean) => ({
-    execute: jest.fn(() => canUndo),
-    undo: jest.fn()
-}));
-
-const MockActionResolverCannotExecute = jest.fn<IActionResolver<GameContext, TestPayload>>(() => ({
-    execute: jest.fn(),
-    canExecute: () => false
-}));
-
-describe("action resolvers", () => {
-    describe("registration", () => {
-        it("gameService throws trying to resolve unregistered action", () => {
-            const gameContext: GameContext = {};
-            const gameService = new GameService<GameContext>(gameContext);
-            const action: IGameAction = {
-                type: "foo",
-                payload: {}
-            };
-            expect(() => {
-                gameService.resolveAction(action);
-            }).toThrow();
-        });
-
-        it("gameService throws when trying to re-register action resolver", () => {
-            const gameContext: GameContext = {};
-            const gameService = new GameService<GameContext>(gameContext);
-
-            const mock = new MockActionResolver();
-            gameService.registerActionResolver("foo", mock);
-            expect(() => {
-                gameService.registerActionResolver("foo", mock);
-            }).toThrow();
-        });
-    });
-    describe("execution", () => {
-        it("gameService calls execute on action resolver for registered actions", () => {
-            const gameContext: GameContext = {};
-            const gameService = new GameService<GameContext>(gameContext);
-
-            const mock = new MockActionResolver();
-            gameService.registerActionResolver("foo", mock);
-            const action: IGameAction = {
-                type: "foo",
-                payload: {}
-            };
-
-            gameService.resolveAction(action);
-            expect(mock.execute).toHaveBeenCalled();
-            expect(gameService.actions[0]).toBe(action);
-        });
-
-        it("gameService canExecute returns true if action resolver does not implement canExecute", () => {
-            const gameContext: GameContext = {};
-            const gameService = new GameService<GameContext>(gameContext);
-
-            const mock = new MockActionResolver();
-            gameService.registerActionResolver("foo", mock);
-            const action: IGameAction = {
-                type: "foo",
-                payload: {}
-            };
-
-            const canExecute = gameService.canExecute(action);
-            expect(canExecute);
-        });
-
-        it("gameService canExecute calls resolver canExecute", () => {
-            const gameContext: GameContext = {};
-            const gameService = new GameService<GameContext>(gameContext);
-
-            const mock = new MockActionResolverCannotExecute();
-            gameService.registerActionResolver("foo", mock);
-            const action: IGameAction = {
-                type: "foo",
-                payload: {}
-            };
-
-            const canExecute = gameService.canExecute(action);
-            expect(canExecute).toBeFalsy();
-        });
-    });
-    describe("undo/redo", () => {
-        it("undoable action gets added to undoable stack on execute", () => {
-            const gameContext: GameContext = {};
-            const gameService = new GameService<GameContext>(gameContext);
-
-            const mock = new MockActionResolver(true);
-            gameService.registerActionResolver("foo", mock);
-            const action: IGameAction = {
-                type: "foo",
-                payload: {}
-            };
-
-            gameService.resolveAction(action);
-
-            expect(gameService.undoable[0]).toBe(action);
-        });
-
-        it("non-undoable action does not get added to stack on execute", () => {
-            const gameContext: GameContext = {};
-            const gameService = new GameService<GameContext>(gameContext);
-
-            const mock = new MockActionResolver(false);
-            gameService.registerActionResolver("foo", mock);
-            const action: IGameAction = {
-                type: "foo",
-                payload: {}
-            };
-
-            gameService.resolveAction(action);
-
-            expect(gameService.undoable.length).toBe(0);
-        });
-
-        it("non-undoable action resets undoable and redoable stacks", () => {
-            const gameContext: GameContext = {};
-            const gameService = new GameService<GameContext>(gameContext);
-
-            const mock = new MockActionResolver(false);
-            gameService.registerActionResolver("foo", mock);
-            const action: IGameAction = {
-                type: "foo",
-                payload: {}
-            };
-
-            gameService.undoable.push(action);
-            gameService.redoable.push(action);
-
-            gameService.resolveAction(action);
-
-            expect(gameService.undoable.length).toBe(0);
-            expect(gameService.redoable.length).toBe(0);
-            expect(gameService.actions.length).toBe(1);
-        });
-
-        it("undo calls undo on resolver, updates actions, and stacks", () => {
-            const gameContext: GameContext = {};
-            const gameService = new GameService<GameContext>(gameContext);
-
-            const mock = new MockActionResolver(true);
-            gameService.registerActionResolver("foo", mock);
-            const action: IGameAction = {
-                type: "foo",
-                payload: {}
-            };
-
-            gameService.actions.push(action);
-            gameService.undoable.push(action);
-
-            gameService.undo();
-
-            expect(mock.undo).toHaveBeenCalled();
-            expect(gameService.undoable.length).toBe(0);
-            expect(gameService.redoable.length).toBe(1);
-            expect(gameService.actions.length).toBe(0);
-        });
-
-        it("redo calls execute on resolver, updates actions, and stacks", () => {
-            const gameContext: GameContext = {};
-            const gameService = new GameService<GameContext>(gameContext);
-
-            const mock = new MockActionResolver(true);
-            gameService.registerActionResolver("foo", mock);
-            const action: IGameAction = {
-                type: "foo",
-                payload: {}
-            };
-
-            gameService.redoable.push(action);
-
-            gameService.redo();
-
-            expect(mock.execute).toHaveBeenCalled();
-            expect(gameService.undoable.length).toBe(1);
-            expect(gameService.redoable.length).toBe(0);
-            expect(gameService.actions.length).toBe(1);
-        });
-    });
+import { IGameAction, GameService, IActionResolver, IActionPayload, IGameContext } from "./gameService";
+
+class TestPayload implements IActionPayload {
+
+}
+
+class GameContext implements IGameContext {
+
+}
+
+const MockActionResolver = jest.fn<IActionResolver<GameContext, TestPayload>>((canUndo?: boolean) => ({
+    execute: jest.fn(() => canUndo),
+    undo: jest.fn()
+}));
+
+const MockActionResolverCannotExecute = jest.fn<IActionResolver<GameContext, TestPayload>>(() => ({
+    execute: jest.fn(),
+    canExecute: () => false
+}));
+
+describe("action resolvers", () => {
+    describe("registration", () => {
+        it("gameService throws trying to resolve unregistered action", () => {
+            const gameContext: GameContext = {};
+            const gameService = new GameService<GameContext>(gameContext);
+            const action: IGameAction = {
+                type: "foo",
+                payload: {}
+            };
+            expect(() => {
+                gameService.resolveAction(action);
+            }).toThrow();
+        });
+
+        it("gameService throws when trying to re-register action resolver", () => {
+            const gameContext: GameContext = {};
+            const gameService = new GameService<GameContext>(gameContext);
+
+            const mock = new MockActionResolver();
+            gameService.registerActionResolver("foo", mock);
+            expect(() => {
+                gameService.registerActionResolver("foo", mock);
+            }).toThrow();
+        });
+    });
+    describe("execution", () => {
+        it("gameService calls execute on action resolver for registered actions", () => {
+            const gameContext: GameContext = {};
+            const gameService = new GameService<GameContext>(gameContext);
+
+            const mock = new MockActionResolver();
+            gameService.registerActionResolver("foo", mock);
+            const action: IGameAction = {
+                type: "foo",
+                payload: {}
+            };
+
+            gameService.resolveAction(action);
+            expect(mock.execute).toHaveBeenCalled();
+            expect(gameService.actions[0]).toBe(action);
+        });
+
+        it("gameService canExecute returns true if action resolver does not implement canExecute", () => {
+            const gameContext: GameContext = {};
+            const gameService = new GameService<GameContext>(gameContext);
+
+            const mock = new MockActionResolver();
+            gameService.registerActionResolver("foo", mock);
+            const action: IGameAction = {
+                type: "foo",
+                payload: {}
+            };
+
+            const canExecute = gameService.canExecute(action);
+            expect(canExecute);
+        });
+
+        it("gameService canExecute calls resolver canExecute", () => {
+            const gameContext: GameContext = {};
+            const gameService = new GameService<GameContext>(gameContext);
+
+            const mock = new MockActionResolverCannotExecute();
+            gameService.registerActionResolver("foo", mock);
+            const action: IGameAction = {
+                type: "foo",
+                payload: {}
+            };
+
+            const canExecute = gameService.canExecute(action);
+            expect(canExecute).toBeFalsy();
+        });
+    });
+    describe("undo/redo", () => {
+        it("undoable action gets added to undoable stack on execute", () => {
+            const gameContext: GameContext = {};
+            const gameService = new GameService<GameContext>(gameContext);
+
+            const mock = new MockActionResolver(true);
+            gameService.registerActionResolver("foo", mock);
+            const action: IGameAction = {
+                type: "foo",
+                payload: {}
+            };
+
+            gameService.resolveAction(action);
+
+            expect(gameService.undoable[0]).toBe(action);
+        });
+
+        it("non-undoable action does not get added to stack on execute", () => {
+            const gameContext: GameContext = {};
+            const gameService = new GameService<GameContext>(gameContext);
+
+            const mock = new MockActionResolver(false);
+            gameService.registerActionResolver("foo", mock);
+            const action: IGameAction = {
+                type: "foo",
+                payload: {}
+            };
+
+            gameService.resolveAction(action);
+
+            expect(gameService.undoable.length).toBe(0);
+        });
+
+        it("non-undoable action resets undoable and redoable stacks", () => {
+            const gameContext: GameContext = {};
+            const gameService = new GameService<GameContext>(gameContext);
+
+            const mock = new MockActionResolver(false);
+            gameService.registerActionResolver("foo", mock);
+            const action: IGameAction = {
+                type: "foo",
+                payload: {}
+            };
+
+            gameService.undoable.push(action);
+            gameService.redoable.push(action);
+
+            gameService.resolveAction(action);
+
+            expect(gameService.undoable.length).toBe(0);
+            expect(gameService.redoable.length).toBe(0);
+            expect(gameService.actions.length).toBe(1);
+        });
+
+        it("undo calls undo on resolver, updates actions, and stacks", () => {
+            const gameContext: GameContext = {};
+            const gameService = new GameService<GameContext>(gameContext);
+
+            const mock = new MockActionResolver(true);
+            gameService.registerActionResolver("foo", mock);
+            const action: IGameAction = {
+                type: "foo",
+                payload: {}
+            };
+
+            gameService.actions.push(action);
+            gameService.undoable.push(action);
+
+            gameService.undo();
+
+            expect(mock.undo).toHaveBeenCalled();
+            expect(gameService.undoable.length).toBe(0);
+            expect(gameService.redoable.length).toBe(1);
+            expect(gameService.actions.length).toBe(0);
+        });
+
+        it("redo calls execute on resolver, updates actions, and stacks", () => {
+            const gameContext: GameContext = {};
+            const gameService = new GameService<GameContext>(gameContext);
+
+            const mock = new MockActionResolver(true);
+            gameService.registerActionResolver("foo", mock);
+            const action: IGameAction = {
+                type: "foo",
+                payload: {}
+            };
+
+            gameService.redoable.push(action);
+
+            gameService.redo();
+
+            expect(mock.execute).toHaveBeenCalled();
+            expect(gameService.undoable.length).toBe(1);
+            expect(gameService.redoable.length).toBe(0);
+            expect(gameService.actions.length).toBe(1);
+        });
+    });
 });
--- src/tictactoe/game.test.ts
+++ src/tictactoe/game.test.ts
@@ -1,50 +1,50 @@
-import { ITTTGameContext, GameState, StartGameResolver, MarkSquareResolver } from "./game";
-import { GameService } from "../game/GameService";
-
-describe("tictactoe", () => {
-    it("full game", () => {
-        const gameContext: ITTTGameContext = {
-            squares: [],
-            currentPlayer: "",
-            state: GameState.PreGame
-        };
-        const gameService = new GameService<ITTTGameContext>(gameContext);
-        const startGameResolver = new StartGameResolver();
-        gameService.registerActionResolver(StartGameResolver.type, startGameResolver);
-        const markSquareResolver = new MarkSquareResolver();
-        gameService.registerActionResolver(MarkSquareResolver.type, markSquareResolver);
-
-        const startAction = StartGameResolver.create(gameContext);
-        gameService.resolveAction(startAction);
-        expect(gameContext.state).toBe(GameState.InGame);
-        expect(gameContext.squares[1][1]).toBe("");
-
-        let markSquareAction = MarkSquareResolver.create(gameContext, 0, 0); // X
-        gameService.resolveAction(markSquareAction);
-
-        markSquareAction = MarkSquareResolver.create(gameContext, 2, 0); // O
-        gameService.resolveAction(markSquareAction);
-
-        markSquareAction = MarkSquareResolver.create(gameContext, 1, 1); // X
-        gameService.resolveAction(markSquareAction);
-
-        markSquareAction = MarkSquareResolver.create(gameContext, 2, 1); // O
-        gameService.resolveAction(markSquareAction);
-
-        markSquareAction = MarkSquareResolver.create(gameContext, 2, 2); // X
-        gameService.resolveAction(markSquareAction);
-
-        expect(gameContext.state).toBe(GameState.PostGame);
-
-        gameService.undo();
-        expect(gameContext.state).toBe(GameState.InGame);
-
-        markSquareAction = MarkSquareResolver.create(gameContext, 0, 1); // X
-        gameService.resolveAction(markSquareAction);
-
-        markSquareAction = MarkSquareResolver.create(gameContext, 2, 2); // O
-        gameService.resolveAction(markSquareAction);
-
-        expect(gameContext.state).toBe(GameState.PostGame);
-    });
+import { ITTTGameContext, GameState, StartGameResolver, MarkSquareResolver } from "./game";
+import { GameService } from "../game/GameService";
+
+describe("tictactoe", () => {
+    it("full game", () => {
+        const gameContext: ITTTGameContext = {
+            squares: [],
+            currentPlayer: "",
+            state: GameState.PreGame
+        };
+        const gameService = new GameService<ITTTGameContext>(gameContext);
+        const startGameResolver = new StartGameResolver();
+        gameService.registerActionResolver(StartGameResolver.type, startGameResolver);
+        const markSquareResolver = new MarkSquareResolver();
+        gameService.registerActionResolver(MarkSquareResolver.type, markSquareResolver);
+
+        const startAction = StartGameResolver.create(gameContext);
+        gameService.resolveAction(startAction);
+        expect(gameContext.state).toBe(GameState.InGame);
+        expect(gameContext.squares[1][1]).toBe("");
+
+        let markSquareAction = MarkSquareResolver.create(gameContext, 0, 0); // X
+        gameService.resolveAction(markSquareAction);
+
+        markSquareAction = MarkSquareResolver.create(gameContext, 2, 0); // O
+        gameService.resolveAction(markSquareAction);
+
+        markSquareAction = MarkSquareResolver.create(gameContext, 1, 1); // X
+        gameService.resolveAction(markSquareAction);
+
+        markSquareAction = MarkSquareResolver.create(gameContext, 2, 1); // O
+        gameService.resolveAction(markSquareAction);
+
+        markSquareAction = MarkSquareResolver.create(gameContext, 2, 2); // X
+        gameService.resolveAction(markSquareAction);
+
+        expect(gameContext.state).toBe(GameState.PostGame);
+
+        gameService.undo();
+        expect(gameContext.state).toBe(GameState.InGame);
+
+        markSquareAction = MarkSquareResolver.create(gameContext, 0, 1); // X
+        gameService.resolveAction(markSquareAction);
+
+        markSquareAction = MarkSquareResolver.create(gameContext, 2, 2); // O
+        gameService.resolveAction(markSquareAction);
+
+        expect(gameContext.state).toBe(GameState.PostGame);
+    });
 });
