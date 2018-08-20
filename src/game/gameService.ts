86081357490486ed47e8d37a2cc5a314876f6362
Derrick Marcey <demarcey@microsoft.com>
2018-08-20 17:08:40 -0400

    First commit.

---
+++ src/game/gameService.ts
@@ -0,0 +1,102 @@
+export interface IActionPayload {
+
+}
+
+export interface IGameContext {
+
+}
+
+export interface IGameAction {
+    type: string;
+    payload: IActionPayload;
+}
+
+export interface IActionResolver<S extends IGameContext, T extends IActionPayload> {
+    execute(gameContext: S, action: T): boolean;
+    canExecute?(gameContext: S, action: T): boolean;
+
+    undo?(gameContext: S, action: T): void;
+}
+
+export interface IGameService<S extends IGameContext> {
+    registerActionResolver<T extends IActionPayload>(actionType: string, resolver: IActionResolver<S, T>): void;
+    resolveAction(action: IGameAction): void;
+    undo(): void;
+    redo(): void;
+
+    canExecute(action: IGameAction): boolean;
+
+    actions: IGameAction[];
+    undoable: IGameAction[];
+    redoable: IGameAction[];
+}
+
+export class GameService<S> implements IGameService<S> {
+    private resolverMap: { [actionType: string]: IActionResolver<S, any> } = {};
+    private gameContext: S;
+
+    public actions: IGameAction[] = [];
+    public undoable: IGameAction[] = [];
+    public redoable: IGameAction[] = [];
+
+    constructor(gameContext: S) {
+        this.gameContext = gameContext;
+    }
+
+    public canExecute(action: IGameAction): boolean {
+        const resolver = this.getResolver(action.type);
+        if (resolver.canExecute) {
+            return resolver.canExecute(this.gameContext, action);
+        }
+        return true;
+    }
+
+    public registerActionResolver<T extends IActionPayload>(actionType: string, resolver: IActionResolver<S, T>): void {
+        const cachedResolver = this.getResolver(actionType);
+        if (cachedResolver) {
+            throw Error(`Action of type ${actionType} already resolved`);
+        }
+        this.resolverMap[actionType] = resolver;
+    }
+
+    public resolveAction(action: IGameAction): void {
+        const resolver = this.getResolver(action.type);
+        const canUndo = resolver.execute(this.gameContext, action.payload);
+
+        this.actions.push(action);
+
+        if (canUndo) {
+            this.undoable.push(action);
+        }
+        else {
+            this.undoable = [];
+            this.redoable = [];
+        }
+    }
+
+    public undo(): void {
+        const action = this.undoable.pop();
+        if (action) {
+            const resolver = this.getResolver(action.type);
+            if (resolver.undo) {
+                resolver.undo(this.gameContext, action.payload);
+                this.actions.pop();
+                this.redoable.push(action);
+            }
+        }
+    }
+
+    public redo(): void {
+        const action = this.redoable.pop();
+        if (action) {
+            const resolver = this.getResolver(action.type);
+            resolver.execute(this.gameContext, action.payload);
+            this.actions.push(action);
+            this.undoable.push(action);
+        }
+    }
+
+    private getResolver<T>(actionType: string): IActionResolver<S, T> {
+        return this.resolverMap[actionType];
+    }
+}
---
+++ src/tictactoe/game.ts
@@ -0,0 +1,117 @@
+import { IGameContext, IActionResolver, IGameAction, IActionPayload } from "../game/GameService";
+
+export const enum GameState {
+    PreGame,
+    InGame,
+    PostGame
+}
+
+export class ITTTGameContext implements IGameContext {
+    squares: string[][];
+    currentPlayer: string;
+    state: GameState;
+}
+
+export interface StartGamePayload extends IActionPayload {
+
+}
+
+export class StartGameResolver implements IActionResolver<ITTTGameContext, StartGamePayload> {
+    public execute(gameContext: ITTTGameContext, action: StartGamePayload) {
+        gameContext.currentPlayer = "X";
+        gameContext.squares = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ];
+        gameContext.state = GameState.InGame;
+        return false;
+    }
+
+    public static type = "START_GAME";
+
+    public static create(gameContext: ITTTGameContext): IGameAction {
+        return {
+            type: StartGameResolver.type,
+            payload: {}
+        };
+    }
+}
+
+export interface MarkSquarePayload {
+    player: string;
+    x: number;
+    y: number;
+}
+
+export class MarkSquareResolver implements IActionResolver<ITTTGameContext, MarkSquarePayload> {
+    public execute(gameContext: ITTTGameContext, action: MarkSquarePayload) {
+        const { x, y, player } = action;
+        gameContext.squares[x][y] = player;
+        if (this.checkForGameEnd(gameContext, player)) {
+            gameContext.state = GameState.PostGame;
+        }
+        else {
+            this.switchPlayer(gameContext);
+        }
+        return true;
+    }
+
+    public undo(gameContext: ITTTGameContext, action: MarkSquarePayload) {
+        const { x, y, player } = action;
+        gameContext.squares[x][y] = "";
+        this.switchPlayer(gameContext);
+        gameContext.state = GameState.InGame;
+    }
+
+    private switchPlayer(gameContext: ITTTGameContext) {
+        gameContext.currentPlayer === "X" ? gameContext.currentPlayer = "O" : gameContext.currentPlayer = "X";
+    }
+
+    private checkForGameEnd(gameContext: ITTTGameContext, player: string) {
+        const { squares } = gameContext;
+
+        for (let i = 0; i < 3; i++) {
+            if (squares[i][0] === player &&
+                squares[i][1] === player &&
+                squares[i][2] === player) {
+                return true;
+            }
+
+            if (squares[0][i] === player &&
+                squares[1][i] === player &&
+                squares[2][i] === player) {
+                return true;
+            }
+        }
+
+        if (squares[0][0] === player &&
+            squares[1][1] === player &&
+            squares[2][2] === player) {
+            return true;
+        }
+
+        if (squares[0][2] === player &&
+            squares[1][1] === player &&
+            squares[2][0] === player) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static type = "MARK_SQUARE";
+
+    public static create(gameContext: ITTTGameContext, x: number, y: number): IGameAction {
+        const player = gameContext.currentPlayer;
+        return {
+            type: MarkSquareResolver.type,
+            payload: {
+                player,
+                x,
+                y
+            }
+        };
+    }
+
+}
